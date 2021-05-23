125ebe0d1e495006f3cf8dbc26387cdbb0d30d4d
Salih ŞENTÜRK <salihsenturk97@gmail.com>
2021-05-23 20:30:01 +0300

    Swagger desteği getirildi

--- Hrms/src/main/java/kodlamaio/Hrms/dataAccess/abstracts/JobTitleDao.java
+++ Hrms/src/main/java/kodlamaio/Hrms/dataAccess/abstracts/JobTitleDao.java
@@ -2,11 +2,12 @@ package kodlamaio.Hrms.dataAccess.abstracts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import kodlamaio.Hrms.entities.concretes.JobTitle;
+
 
 public interface JobTitleDao extends JpaRepository<JobTitle, Integer>{
 	
 	// OLUŞTURMA SİLME GÜNCELLEME OLAYINI Jpa yapıyor, bu methodları Jpa sağlıyor
-
-		
+	
 }
